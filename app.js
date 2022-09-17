70c32368c4bb319904d14cf8a3f8dad6d073c87e
Yoga Adi Saputra <syogaadi75@gmail.com>
2022-09-17 18:57:56 +0700

    Connect Episode Schema with Film Schema

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose')
 const app = express()
 const bodyParser = require('body-parser')
 const verifyToken = require('./middleware/verifyToken')
+const cors = require('cors')
 require('dotenv/config')
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
 

067dffb29248f8429641a596c6b77bed8edc2a69
Yoga Adi Saputra <syogaadi75@gmail.com>
2022-09-20 11:20:00 +0700

    Add histories

--- routes/histories.js
+++ routes/histories.js
@@ -3,6 +3,8 @@ const express = require('express')
 const router = express.Router();
 const mongoose = require('mongoose');
 const Histories = require('../models/Histories');
+const Episode = require('../models/Episode');
+const Film = require('../models/Film');
 
 // Get
 router.get('/:dataIp', async (req, res) => {
@@ -18,7 +20,7 @@ router.get('/:dataIp', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const history = Histories.create({ ip: req.body.ip })
+        const history = await Histories.create({ ip: req.body.ip })
         res.send(history)
     } catch (error) {
         res.send({
@@ -32,14 +34,18 @@ router.put('/:id', async (req, res) => {
         const cekHistory = await Histories.findOne({ $and: [{ _id: req.params.id }, { 'episodes._id': req.body.episode }] })
         if (!cekHistory) {
             const dataHistory = await Histories.findOne({ _id: req.params.id })
-            if (dataHistory.episodes.length >= 12) {
+            if (dataHistory.episodes.length >= 14) {
                 await Histories.updateOne({ _id: req.params.id }, { $pull: { episodes: { _id: dataHistory.episodes[0]._id } } })
             }
 
+            const dataEps = await Episode.findOne({ _id: req.body.episode })
+            const dataFilm = await Film.findOne({ _id: dataEps.id_film })
             const addHistory = await Histories.updateOne({ _id: req.params.id }, {
                 $push: {
                     episodes: {
                         _id: req.body.episode,
+                        episode: dataEps,
+                        film: dataFilm,
                         insertAt: Date()
                     }
                 }
